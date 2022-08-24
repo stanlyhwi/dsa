a50808326c3a8ac87b537ef83d187dbf2e47bf7a
stanlyhwi <111750427+stanlyhwi@users.noreply.github.com>
2022-08-24 15:04:47 +0530

    Add files via upload

---
+++ recursive.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class recursive{
+    public static int rec(int arr[],int coin,int max,int count,int me){
+        while(coin!=0){
+            if(coin>=max){
+                coin=coin-max;
+                count=count+1;
+            }else{
+                me=me-1;
+                max=arr[me];
+            }
+            if(coin!=0){
+                rec(arr,coin,max,count,me);
+            }
+        }
+
+       return count;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        //int n=sc.nextInt();
+        int arr[] =  {1,2,5};
+    
+
+       Arrays.sort(arr);
+
+       int max= arr.length-1;
+       int coin = sc.nextInt();
+       int count=0;
+       int me = arr[arr.length-1];
+
+
+       System.out.println(rec(arr,coin,max,count,me) );
+    }
+}
